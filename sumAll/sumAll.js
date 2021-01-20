90e6518bcd17e58dea65137a14a074ba8b054751
Mike Shine <michaelbshine@outlook.com>
2021-01-20 11:07:26 -0800

    completed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if (num1 < 0 || num2 < 0 || typeof(num1) !== 'number' || typeof     (num2) !== 'number') {
+    return 'ERROR';
+  }
+  const whichIsGreater = Boolean(num1 < num2);
+  let sum = 0;
+  let smallerNumber, biggerNumber;
+  if (whichIsGreater) {
+    smallerNumber = num1;
+    biggerNumber = num2;
+  } else {
+    smallerNumber = num2;
+    biggerNumber = num1;
+  }
+  for (let i = smallerNumber; i <= biggerNumber; i++) {
+    sum += i;
+  }
+  return sum;
 }
 
 module.exports = sumAll
