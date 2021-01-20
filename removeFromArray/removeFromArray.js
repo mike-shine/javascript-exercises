3e6b91e9e8d3804b12f2d97eb811670132050e78
Mike Shine <michaelbshine@outlook.com>
2021-01-20 10:55:19 -0800

    completed removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...arguments) {
+  const array = arguments[0];
+  for (let i = 0; i < (arguments.length); i++) {
+    const locationOfValue = array.indexOf(arguments[i]);
+    if (locationOfValue < 0) {
+      continue;
+    }
+    array.splice(locationOfValue, 1)
+  }
 
+  return array;
 }
 
 module.exports = removeFromArray
