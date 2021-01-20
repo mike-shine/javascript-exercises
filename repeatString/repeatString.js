10a5e70ab88a4da4745068473fbecd1035fcbd71
Mike Shine <michaelbshine@outlook.com>
2021-01-19 18:05:20 -0800

    completed repeatString and reverseString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+  if (number < 0) {
+    return 'ERROR';
+  }
+  let resultString = '';
+  let i = 0;
+  while (i < number) {
+    resultString += string;
+    i++;
+  }
+  return resultString;
 }
 
 module.exports = repeatString
