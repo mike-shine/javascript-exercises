c719cb540d021bc3e20ede15cf5ab26cc8a868bf
Mike Shine <michaelbshine@outlook.com>
2021-02-01 14:23:46 -0800

    completed fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(target) {
+  if (target < 0) {
+    return 'OOPS';
+  }
+  if (typeof target === 'string') {
+    target = Number(target);
+  }
+  const fibArray = [1, 1];
+  while (fibArray.length <= target) {
+    fibArray.push(fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2]);
+  }
+  return fibArray[fibArray.length - 2];
 }
 
 module.exports = fibonacci
