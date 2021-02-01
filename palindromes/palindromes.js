40f8dc3e756d530a8030d213c4968e4fb4813d2a
Mike Shine <michaelbshine@outlook.com>
2021-02-01 11:43:39 -0800

    finished palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+  let newString = '';
+  [...string].forEach(character => {
+    if (character === ',' || character === '.' || character === ' ' || character === '!') {
+      return;
+    } else {
+      newString += character.toLowerCase();
+    }
+  });
+  let reverseString = newString.split('').reverse().join('');
+  return newString === reverseString;
 }
 
 module.exports = palindromes
