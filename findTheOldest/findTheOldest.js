c97767a515a9385061218c5d697c0f4d01c1fe08
Mike Shine <michaelbshine@outlook.com>
2021-02-01 15:40:40 -0800

    completed findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+  return people.reduce((oldest, current) => {
+    let oldestAge = findAge(oldest.yearOfBirth, oldest.yearOfDeath)
+    let currentAge = findAge(current.yearOfBirth, current.yearOfDeath)
+    return currentAge > oldestAge ? current : oldest;
+  })
 
+  function findAge(birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  }
 }
 
 module.exports = findTheOldest
